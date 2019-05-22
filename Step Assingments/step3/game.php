8b3068a85f19f2275607a37e3ca7c1fd6774447d
Tony Sulfaro <tony116523@gmail.com>
2019-05-21 21:55:55 -0700

    bird and pit logic implemented
    working on wumpus logic for 2 caves away

--- Step Assingments/step3/game.php
+++ Step Assingments/step3/game.php
@@ -1,6 +1,27 @@
 <?php
 require 'format.inc.php';
 require 'wumpus.inc.php';
+
+$room = 1; // Starting room
+$birds = 7;  // Room with the birds
+$wumpus = 16; // wumpus location, BEWARE
+$pits = array(3, 10, 13);    // Rooms with a bottomless pit
+
+$cave = cave_array();
+if(isset($_GET['r'])) {
+    // We have been passed a room number
+    $room = $_GET['r'];
+}
+
+// move player from bird room 7 to room 10
+if($room == 7){
+    $room = 10;
+}
+
+// player dies when entering pit
+if(in_array($room, $pits) or $room == $wumpus){
+    header("Location: lose.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,27 +40,44 @@ require 'wumpus.inc.php';
         <?php
         echo '<p>' . date("g:ia l, F j, Y") . '</p>';
         ?>
-        <p id="room-number">You are in room 6</p>
-        <p>You hear birds!</p>
-        <p>You feel a draft!</p>
-        <p>You smell a wumpus!</p>
+        <p id="room-number">You are in room <?php echo $room; ?></p>
+
+        <?php
+        //check if birds room is adjacent
+        for($i=0; $i<=2; $i++){
+            if($cave[$room][$i] == $birds){
+                echo "<p>You hear birds!</p>";
+            }
+        }
+        ?>
+
+        <?php
+        //check if pit is adjacent
+        for($i=0; $i<=2; $i++){
+            if(in_array($cave[$room][$i],$pits)){
+                echo "<p>You feel a draft!</p>";
+            }
+        }
+        ?>
+
+<!--        <p>You smell a wumpus!</p>-->
 
         <div class="rooms">
             <div class="room">
                 <img src="images/cave2.jpg" alt="cave2">
-                <p><a href="">3</a></p>
+                <p><a href="game.php?r=<?php echo $cave[$room][0]; ?>"><?php echo $cave[$room][0]; ?></a></p>
                 <p><a href="">Shoot Arrow</a></p>
             </div>
 
             <div class="room">
                 <img src="images/cave2.jpg" alt="cave2">
-                <p><a href="">13</a></p>
+                <p><a href="game.php?r=<?php echo $cave[$room][1]; ?>"><?php echo $cave[$room][1]; ?></a></p>
                 <p><a href="">Shoot Arrow</a></p>
             </div>
 
             <div class="room">
                 <img src="images/cave2.jpg" alt="cave2">
-                <p><a href="">7</a></p>
+                <p><a href="game.php?r=<?php echo $cave[$room][2]; ?>"><?php echo $cave[$room][2]; ?></a></p>
                 <p><a href="">Shoot Arrow</a></p>
             </div>
         </div>
--- Step Assingments/step3/wumpus.inc.php
+++ Step Assingments/step3/wumpus.inc.php
@@ -1,11 +1,28 @@
 <?php
 
 function cave_array() {
-    $cave = array(1 => array(5, 6, 2),
-        2 => array(1, 8, 3)
+    $cave = array(
+        1 => array(5, 6, 2),
+        2 => array(1, 8, 3),
+        3 => array(4, 10, 2),
+        4 => array(5, 12, 3),
+        5 => array(1, 14, 4),
+        6 => array(7, 1, 15),
+        7 => array(8, 16, 6),
+        8 => array(9, 2, 7),
+        9 => array(10, 17, 8),
+        10 => array(11, 3, 9),
+        11 => array(12, 18, 10),
+        12 => array(13, 4, 11),
+        13 => array(14, 9, 12),
+        14 => array(15, 5, 3),
+        15 => array(6, 20, 14),
+        16 => array(20, 7, 17),
+        17 => array(16, 9, 18),
+        18 => array(17, 11, 19),
+        19 => array(18, 13, 20),
+        20 => array(16, 15, 19),
     );
 
     return $cave;
 }
-
-print_r(cave_array());
