eb46a196815fde7943d69b40bb3d5cf74774a075
Tony Sulfaro <tony116523@gmail.com>
2019-05-21 22:19:26 -0700

    finished wumpus game

--- Step Assingments/step3/game.php
+++ Step Assingments/step3/game.php
@@ -6,11 +6,18 @@ $room = 1; // Starting room
 $birds = 7;  // Room with the birds
 $wumpus = 16; // wumpus location, BEWARE
 $pits = array(3, 10, 13);    // Rooms with a bottomless pit
-
 $cave = cave_array();
+
 if(isset($_GET['r'])) {
     // We have been passed a room number
     $room = $_GET['r'];
+}
+
+if(isset($_GET['a'])){
+    if($_GET['a'] == 16){
+        header("Location: win.php");
+        exit;
+    }
 }
 
 // move player from bird room 7 to room 10
@@ -21,6 +28,7 @@ if($room == 7){
 // player dies when entering pit
 if(in_array($room, $pits) or $room == $wumpus){
     header("Location: lose.php");
+    exit;
 }
 ?>
 <!DOCTYPE html>
@@ -83,19 +91,19 @@ if(in_array($room, $pits) or $room == $wumpus){
             <div class="room">
                 <img src="images/cave2.jpg" alt="cave2">
                 <p><a href="game.php?r=<?php echo $cave[$room][0]; ?>"><?php echo $cave[$room][0]; ?></a></p>
-                <p><a href="">Shoot Arrow</a></p>
+                <p><a href="game.php?r=<?php echo $room; ?>&a=<?php echo $cave[$room][0] ?>">Shoot Arrow</a></p>
             </div>
 
             <div class="room">
                 <img src="images/cave2.jpg" alt="cave2">
                 <p><a href="game.php?r=<?php echo $cave[$room][1]; ?>"><?php echo $cave[$room][1]; ?></a></p>
-                <p><a href="">Shoot Arrow</a></p>
+                <p><a href="game.php?r=<?php echo $room; ?>&a=<?php echo $cave[$room][1] ?>">Shoot Arrow</a></p>
             </div>
 
             <div class="room">
                 <img src="images/cave2.jpg" alt="cave2">
                 <p><a href="game.php?r=<?php echo $cave[$room][2]; ?>"><?php echo $cave[$room][2]; ?></a></p>
-                <p><a href="">Shoot Arrow</a></p>
+                <p><a href="game.php?r=<?php echo $room; ?>&a=<?php echo $cave[$room][2] ?>">Shoot Arrow</a></p>
             </div>
         </div>
 
