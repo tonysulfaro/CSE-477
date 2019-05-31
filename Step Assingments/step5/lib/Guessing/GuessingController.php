06ca71e8ac359d8134797cfc0a541118ff826330
Tony Sulfaro <tony116523@gmail.com>
2019-05-31 15:33:47 -0700

    Create GuessingController.php

---
+++ Step Assingments/step5/lib/Guessing/GuessingController.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/lib/guessing.inc.php';
+$controller = new Guessing\GuessingController($guessing, $_POST);
+if($controller->isReset()) {
+    unset($_SESSION[GUESSING_SESSION]);
+}
+header("location: guessing.php");
+exit;
