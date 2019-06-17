10a337bbe9c45bfe216cec4ea0151f7877e8d902
Tony Sulfaro <tony116523@gmail.com>
2019-06-17 10:28:54 -0700

    Update page.js

--- Step Assingments/step9/quizzes/quizforms/page.js
+++ Step Assingments/step9/quizzes/quizforms/page.js
@@ -1,14 +1,16 @@
 var number = Math.floor(Math.random() * 100);
-console.log(number);
+console.log(number); 
 
 window.onload = function () {
+
+
 
     // override form default function
     document.getElementById('form').onsubmit = function (event) {
         var guess = document.getElementById('guess').value;
-        console.log(guess);
+        console.log("guess:"+guess);
 
-        console.log(typeof(guess));
+        console.log("number:"+number);
 
         if (guess === null || guess === '') {
             document.getElementById('result').innerText = 'Silly you, enter a number!';
@@ -24,17 +26,20 @@ window.onload = function () {
             }
             if (guess === number) {
                 document.getElementById('result').innerText = "You're a winner!";
+                console.log("win");
                 number = Math.floor(Math.random() * 100);
             }
         }
         event.preventDefault();
     };
 
-    document.getElementById('giveup').addEventListener('click', function(event){
-        document.getElementById('result').innerText = "The number was "+number+", dood. It's not that hard.";
-        number = Math.floor(Math.random() * 100);
-        console.log(number);
-        event.preventDefault();
-    });
+    
 
 };
+
+function giveUp(){
+    document.getElementById('result').innerText = "The number was "+number+", dood. It's not that hard.";
+        number = Math.floor(Math.random() * 100);
+        console.log(number);
+      
+        }
