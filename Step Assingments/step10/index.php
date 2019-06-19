d5f30a74421ef9542e6ccf2bee8284bc49544e7d
Tony Sulfaro <tony116523@gmail.com>
2019-06-18 21:50:34 -0700

    step10 base files, add webpack and lot of node modules because why not

---
+++ Step Assingments/step10/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>test!</title>
+    <script src="dist/main.js"></script>
+</head>
+<body>
+
+<h1>Test!</h1>
+
+</body>
+</html>
